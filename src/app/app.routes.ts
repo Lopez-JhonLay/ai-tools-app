3c528d9082d3d30a3fb27e97df33aeef86a7fd9f
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-11-05 15:45:40 +0800

    create page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
