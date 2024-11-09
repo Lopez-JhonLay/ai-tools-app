8828d7add93fbc5be9bdef6cb6c15533b5dccfe0
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-11-09 14:08:49 +0800

    add validation to input

--- src/app/components/partials/mood-tunes-input/mood-tunes-input.component.ts
+++ src/app/components/partials/mood-tunes-input/mood-tunes-input.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mood-tunes-input',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './mood-tunes-input.component.html',
   styleUrl: './mood-tunes-input.component.css'
 })
 export class MoodTunesInputComponent {
+  moodInput = new FormControl('');
 
+  clearInput() {
+    this.moodInput.reset();
+  }
+
+  onSubmit() {
+    console.log(this.moodInput.value);
+    this.clearInput();
+  }
 }
